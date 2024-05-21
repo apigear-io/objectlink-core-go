ba3879ace86c2175edaf20082d12a4f113208db2
Jürgen Ryannel <juergen@ryannel.org>
2024-05-21 17:34:55 +0200

    fix ws origin issue

--- olink/ws/hub.go
+++ olink/ws/hub.go
@@ -11,7 +11,11 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 // Hub maintains the set of active peers
 // and broadcasts messages to the peers.
